f32b32c2d984e5dc3409ab5eacb3c25e1e0315fc
LukasOfLockless <71017727+LukasOfLockless@users.noreply.github.com>
2022-12-10 23:21:32 +0200

    renamed to make smt a bit more proper

--- compilerun.bat
+++ compilerun.bat
@@ -1,1 +1,1 @@
-C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe /t:exe /out:compiled.exe main.cs smpl\mkbmp.cs realdata\vis2.cs 
+C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe /t:exe /out:compiled.exe main.cs visualisation1.cs
